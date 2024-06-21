8c4ad441c5047da2eaf2277590050426ba830b76
kanchanapujitha <21951a05e1@iare.ac.in>
2024-06-21 12:16:23 +0530

    Last Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,9 +11,9 @@ const Navbar = () => {
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]"/>
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav,index)=>(
-          <li
-            key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px]  ${index===navLinks.length-1? 'mr-0':'mr-10'} text-white`}>
-            <a href={`#${nav.id}`}>
+          <li 
+            key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index===navLinks.length-1? 'mr-0':'mr-10'} text-white ` }>
+            <a className="hover:text-blue-300" href={`#${nav.id}`}>
               {nav.title}
             </a>
           </li>
@@ -33,8 +33,8 @@ const Navbar = () => {
           <ul className="list-none flex flex-col justify-end items-center flex-1">
           {navLinks.map((nav,index)=>(
             <li
-              key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px]  ${index===navLinks.length-1? 'mr-0':'mr-4'} text-white`}>
-              <a href={`#${nav.id}`}>
+              key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px]   ${index===navLinks.length-1? 'mr-0':'mr-4'} text-white`}>
+              <a className="hover:text-blue-300" href={`#${nav.id}`}>
                 {nav.title}
               </a>
             </li>
